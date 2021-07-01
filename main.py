c1b029061b09550404461b7a065e566a2d488bcb
chien <chiennnm456@gmail.com>
2021-07-01 20:06:53 +0700

    can play musci > 5'

--- main.py
+++ main.py
@@ -33,15 +33,6 @@ async def start(ctx):
 
 @bot.command()
 async def play(ctx, url):
-    song = os.path.isfile('song.mp3')
-
-    try:
-        if song:
-            os.remove('song.mp3')
-    except PermissionError:
-        await ctx.send('Lỗi rồi thằng ngáo đá, lỗi này tao chưa biết sửa')
-        return
-
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     if voice == None:
         await connect(ctx)
@@ -56,13 +47,10 @@ async def play(ctx, url):
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
 
-    for file in os.listdir('./'):
-        if file.endswith('.mp3'):
-            os.rename(file, 'song.mp3')
-
-    voice.play(discord.FFmpegPCMAudio('song.mp3'))
+    voice.play(discord.FFmpegPCMAudio(URL))
 
 
 @bot.command()
